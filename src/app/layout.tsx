f055c007af28272b334df966ee7c2c1abe4bc501
CognativeFarooq <cognativeit@gmail.com>
2025-04-07 10:10:17 +0500

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import "./globals.css";
+//==================
+import ConditionalLayout from "./Layout/conditionalLayout";
+
+//========= Meta Data
+export const metadata: Metadata = {
+  // Title & Descriptions
+  title: "",
+  description: "",
+  // Canonical
+  alternates: { canonical: 'https://ai-pro-resume-next.vercel.app/' },
+  // OG Metas
+  openGraph: {
+    title: "",
+    description: "",
+    url: 'https://ai-pro-resume-next.vercel.app/',
+    siteName: 'Ai Pro Resume',
+    locale: 'en_US',
+    type: 'website',
+  },
+  //===== No-Index =====
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: false,
+      'max-snippet': -1,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+    },
+  }
+}
+export default function RootLayout({ children }: any) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+} 
