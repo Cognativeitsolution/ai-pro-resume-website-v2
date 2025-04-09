d3ef3915b28984adeb85841d30780238e93ff7a6
CognativeFarooq <cognativeit@gmail.com>
2025-04-09 17:54:37 +0500

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Header } from "@/components";
 import Image from "next/image";
-
+//==================
+import { Header } from "@/components";
 //==================
 import BannerBg from "/public/images/bannerBg.svg";
 //========= Meta Data
 export const metadata: Metadata = {
   // Title & Descriptions
-  title: "",
-  description: "",
+  title: "Unlock Global Job Opportunities with AI Pro Resume",
+  description: "Feeling stuck in your job search? Let AI Pro Resume guide you! Create a professional resume and submit it to over 100 global companies awaiting your application.",
   // Canonical
-  alternates: { canonical: "https://ai-pro-resume-next.vercel.app/" },
+  alternates: { canonical: "https://ai-pro-resume-website-v2.vercel.app/" },
   // OG Metas
   openGraph: {
-    title: "",
-    description: "",
-    url: "https://ai-pro-resume-next.vercel.app/",
+    title: "Unlock Global Job Opportunities with AI Pro Resume",
+    description: "Feeling stuck in your job search? Let AI Pro Resume guide you! Create a professional resume and submit it to over 100 global companies awaiting your application.",
+    url: "https://ai-pro-resume-website-v2.vercel.app/",
     siteName: "Ai Pro Resume",
     locale: "en_US",
     type: "website",
@@ -45,7 +45,6 @@ export default function RootLayout({ children }: any) {
           fill={true}
           className="block -z-10 object-cover object-bottom"
         />
-
         <Header />
         {children}
       </body>
