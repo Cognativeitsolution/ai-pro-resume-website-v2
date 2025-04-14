c8d34f84dfd6c34d9ffab448d7eb30c3024a670e
cognative-farooq <farooq.cognativeit@gmail.com>
2025-04-14 18:02:03 +0500

    CTA Component

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -14,7 +14,7 @@ const Welcome = () => {
                     <p className="text-[12px] sm:text-[14px] md:text-[16px] text-justify md:text-left mb-5">where your privacy is our top priority. In this era of rapid technological advancements, safeguarding your personal information is more critical than ever. At AI Pro Resume, we take this responsibility seriously and have crafted a comprehensive Privacy Policy to outline how we handle, protect, and utilize your data. We appreciate your trust in us, and our commitment extends beyond privacy to providing valuable services like Professional resume help, leveraging the capabilities of our AI resume generator and Resume helper.</p>
                     <p className="text-[12px] sm:text-[14px] md:text-[16px] text-justify md:text-left">As you explore our platform, you'll find a wealth of resources, including free resume templates and resume format examples to assist you in crafting a compelling professional narrative. We invite you to delve into the details of our Privacy Policy and discover how AI Pro Resume can be your trusted partner in protecting your privacy and advancing your career with our array of professional services.</p>
                 </div>
-                <div className="flex flex-col justify-center items-center gap-2 my-5">
+                {/* <div className="flex flex-col justify-center items-center gap-2 my-5">
                     <CTA
                         number
                         text="Call Now"
@@ -44,7 +44,7 @@ const Welcome = () => {
                         txtColor="text-hamzaPrimary hover:text-white"
                         border="border-0"
                     />
-                </div>
+                </div> */}
             </section>
         </>
     );
