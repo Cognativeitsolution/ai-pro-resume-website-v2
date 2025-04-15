0aaa37fb371a402d9eaa7bde6435f4e06ed7700d
RabiaSid <rabiasid984@gmail.com>
2025-04-15 09:51:56 +0500

    add

--- src/components/contactUs/CountactUs.tsx
+++ src/components/contactUs/CountactUs.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TfiEmail } from 'react-icons/tfi'
 
 export default function CountactUs() {
     return (
@@ -9,8 +10,14 @@ export default function CountactUs() {
                         <div className='col-span-4 bg-white/30 backdrop-blur-none border-2 border-rose-950'>
                             <h5>Contact Info</h5>
                             <div className=''>
-                                <div className=''>
-                                    kvlkxcjklj
+                                <div className='flex items-center justify-start gap-4'>
+                                    <div className='bg-hamzaPrimary/30 backdrop-blur-sm p-3 rounded-lg'>
+                                        <TfiEmail size={40} className='text-hamzaPrimary' />
+                                    </div>
+                                    <div>
+                                        <h6 className='leading-none'>Email</h6>
+                                        <p>rabiasid@gmail.com</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -16,12 +16,12 @@ const ServiceSection = dynamic(() => import("./serviceSection/ServiceSection"));
 const Reviews = dynamic(() => import("./reviews/Reviews"));
 const Faq = dynamic(() => import("./faq/Faq"));
 const InnerBanner = dynamic(() => import("./innerBanner/InnerBanner"));
-const Features = dynamic(() => import("./Features/Features"));
+const Features = dynamic(() => import("./features/Features"));
 const Counter = dynamic(() => import("./counter/Counter"));
 const Stepper = dynamic(() => import("./stepper/Stepper"));
 const Client = dynamic(() => import("./client/Client"));
 const Welcome = dynamic(() => import("./welcome/Welcome"));
-const PrivacyFeatures = dynamic(() => import("./Features/PrivacyFeatures"));
+const PrivacyFeatures = dynamic(() => import("./features/PrivacyFeatures"));
 const ContactUs = dynamic(() => import("./contactUs/CountactUs"));
 
 export {
