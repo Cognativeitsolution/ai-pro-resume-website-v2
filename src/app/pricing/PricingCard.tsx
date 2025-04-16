f592150e9a9b39d1b4ed46bb24688a86325f2c72
Cognative <hamzvarin@outlook.com>
2025-04-16 15:36:46 +0500

    pricing page 16-5-25

--- src/app/pricing/PricingCard.tsx
+++ src/app/pricing/PricingCard.tsx
@@ -1,17 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./pricing.module.css";
 import check_1 from "../../../public/images/check_1.svg";
+import { CTA } from "@/components";
 
 const packages = [
   {
-    name: "Free",
+    Pckgname: "Free",
     price: "Life Time Access",
-    backgroundColor: "#2f2f2fd1",
-    tickImage: "",
-    color: "text-black",
-    ctaColor: "text-black",
-    titleColor: "text-black",
+    mblSpc: "my-10 md:my-0",
     features: [
       "Unlimited PDF Downloads",
       "Spell & Grammar Checker (5 Tries)",
@@ -27,32 +26,11 @@ const packages = [
       "Manual Cover Letter Generation",
       { text: "Invisible Point", isVisible: true },
     ],
-    buttons: [
-      {
-        text: "012-345-6789",
-        color: "bg-prime text-white border-0",
-        image: "",
-        href: "tel:012-345-6789",
-        imgColor:
-          "bg-white w-[25px] h-[25px] xl:w-[28px] xl:h-[28px] rounded-full p-2 ms-1",
-      },
-      {
-        text: "Live Chat",
-        color: "bg-black text-white border border-white",
-        image: "",
-        href: "javascript:$zopim.livechat.window.show();",
-      },
-    ],
   },
   {
-    name: "Most Popular",
+    Pckgname: "Most Popular",
     price: "1-MONTH/ $ 15.59",
-    backgroundColor: "#262626",
-    color: "text-white",
-    tickImage: "",
     mblSpc: "my-10 md:my-0",
-    ctaColor: "text-white",
-    titleColor: "text-white",
     features: [
       "Spell & Grammar Checker (20 Tries)",
       "6 Fonts & Color Themes",
@@ -67,31 +45,11 @@ const packages = [
       "Share with AI Pro Resume Branding",
       "Unlimited downloads Without AI Pro Resume Branding",
     ],
-    buttons: [
-      {
-        text: "012-345-6789",
-        color: "bg-white text-black font-semibold border-0",
-        image: "",
-        href: "tel:012-345-6789",
-        imgColor:
-          "bg-black w-[25px] h-[25px] xl:w-[28px] xl:h-[28px] rounded-full p-[8.5px] rotate-[90deg]",
-      },
-      {
-        text: "Live Chat",
-        color: "bg-black text-white border border-white",
-        image: "",
-        href: "javascript:$zopim.livechat.window.show();",
-      },
-    ],
   },
   {
-    name: "Premium",
+    Pckgname: "Premium",
     price: "$6-MONTHS / $ 29.99",
-    backgroundColor: "#2f2f2fd1",
-    color: "text-black",
-    tickImage: "",
-    ctaColor: "text-black",
-    titleColor: "text-black",
+    mblSpc: "my-10 md:my-0",
     features: [
       "5 Premium Resume & Cover Letter Templates",
       "Unlimited Resume & Cover Letter Examples",
@@ -107,41 +65,33 @@ const packages = [
       "One-Time Payment (No Need to Cancel)",
       "Share/Download Without AI Pro Resume Branding",
     ],
-    buttons: [
-      {
-        text: "012-345-6789",
-        color: "bg-prime text-white border-0",
-        image: "",
-        href: "tel:012-345-6789",
-        imgColor:
-          "bg-white w-[25px] h-[25px] xl:w-[28px] xl:h-[28px] rounded-full p-2 ms-1",
-      },
-      {
-        text: "Live Chat",
-        color: "bg-black text-white border border-white",
-        image: "",
-        href: "javascript:$zopim.livechat.window.show();",
-      },
-    ],
   },
 ];
 
-type PricingCardProps = {
-  content: {
-    title: string;
-    para: string;
+const PricingCard = () => {
+  const [packageDetailText, setPackageDetailText] = useState("");
+
+  const handlePackageDetail = (Pckgname: any) => {
+    if (Pckgname === "Free") {
+      setPackageDetailText(
+        "This package is perfect for those just starting and looking for essential tools to create or enhance their resumes and cover letters. Enjoy unlimited PDF downloads, 5 attempts at spell and grammar checks, and the ability to edit and share your documents. With access to basic resume sections, templates, examples, and manual cover letter generation, you can get started without any extra cost."
+      );
+    } else if (Pckgname === "Premium") {
+      setPackageDetailText(
+        "Our Premium Package offers the ultimate experience for job seekers. You’ll have access to 5 premium templates, unlimited examples, and 50 tries for spell and grammar checks. Enjoy the convenience of unlimited downloads, access to global job opportunities, and real-time AI suggestions. With 20 resume parser tokens, limitless customization options, and branding-free downloads, this plan ensures you stand out."
+      );
+    } else {
+      setPackageDetailText(
+        "It is ideal for professionals aiming to create standout resumes and cover letters with enhanced features. With spell and grammar check attempts and access to advanced AI chat support, you’ll have more tools to refine your documents. This package includes two premium templates and AI-based cover letter generation. Additionally, enjoy resume parser tokens that allow you to create tailored job applications easily."
+      );
+    }
   };
-};
-
-const PricingCard: React.FC<PricingCardProps> = ({ content }) => {
-  const { title, para } = content;
-
   return (
     <section className="py-5 md:py-10">
       <div className="container">
         <div className="flex items-center justify-center">
           <div className="w-full">
-            <div className={styles.cardsMainDiv}>
+            <div className={`${styles.cardsMainDiv}`}>
               {packages.map((pkg, index) => (
                 <div
                   key={index}
@@ -150,25 +100,28 @@ const PricingCard: React.FC<PricingCardProps> = ({ content }) => {
                   }`}
                 >
                   <div
-                    className={`relative flex flex-col justify-center py-5 h-auto rounded-[35px] border-2 border-white bg-indigo-200/20 backdrop-blur-none`}
+                    className={`relative flex flex-col justify-center py-5 h-auto rounded-[20px] border-2 border-white bg-indigo-200/20 backdrop-blur-none`}
                   >
                     <div className={`px-6 pb-4`}>
                       <h3
                         className={`text-[30px] font-bold leading-tight mb-2`}
                       >
-                        {pkg.name}
+                        {pkg.Pckgname}
                       </h3>
                       <h4 className={`text-[20px] font-bold`}>{pkg.price}</h4>
                     </div>
-                    <div className="flex-wrap flex items-center justify-between mt-[10px] w-[90%] mx-auto">
-                      <button
-                        className={`flex items-center justify-center text-[11px] xl:text-[15px] font-semibold py-[12px] px-[10px] lg:px-[15px] tracking-[.3px] rounded-[5px] leading-[20px] w-full bg-PrimaryDark text-white hover:animate-float hover:bg-primary`}
-                      >
-                        Get Started
-                      </button>
+                    <div className="flex items-center justify-center mt-[10px] w-[90%] mx-auto">
+                      <CTA
+                        btn
+                        text="Get Started"
+                        txtColor="text-white"
+                        bgColor="bg-PrimaryDark hover:bg-primary"
+                        border="border-0"
+                        width="w-full"
+                      />
                     </div>
                     <ul
-                      className={`pt-4 px-6 max-h-[320px] overflow-y-scroll ${styles.redscrollbar}`}
+                      className={`mt-4 px-6 max-h-[320px] overflow-y-scroll ${styles.redscrollbar}`}
                     >
                       {pkg.features.map((feature, i) =>
                         typeof feature === "string" ? (
@@ -190,12 +143,27 @@ const PricingCard: React.FC<PricingCardProps> = ({ content }) => {
                         )
                       )}
                     </ul>
+                    <div className="flex items-center justify-center mt-[30px]">
+                      <CTA
+                        btn
+                        text="View Details"
+                        txtColor="text-white"
+                        bgColor="bg-PrimaryDark hover:bg-primary"
+                        border="border-0"
+                        handleClick={() => handlePackageDetail(pkg.Pckgname)}
+                      />
+                    </div>
                   </div>
                 </div>
               ))}
             </div>
           </div>
         </div>
+        {packageDetailText && (
+          <div className="mt-8 text-black font-medium text-[15px] mx-auto text-center w-[100%] sm:w-[80%] bg-indigo-200/20 border-2 border-white rounded-xl p-2 tracking-wider">
+            {packageDetailText}
+          </div>
+        )}
       </div>
     </section>
   );
