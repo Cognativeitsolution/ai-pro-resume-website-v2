73d5a942088530be71714a4695e90281f95de242
RabiaSid <rabiasid984@gmail.com>
2025-04-17 15:10:26 +0500

    responsiveness

--- src/components/blog/BlogDetail.tsx
+++ src/components/blog/BlogDetail.tsx
@@ -18,13 +18,13 @@ const BlogDetail = () => {
     return (
         <section className="py-5 md:py-10">
             <div className="container">
-                <div className='grid grid-cols-12 gap-6'>
+                <div className='grid lg:grid-cols-12 gap-4 xl:gap-6'>
 
-                    <div className='col-span-4'>
+                    <div className='lg:col-span-5 xl:col-span-4'>
                         <VerticalTabs tabsData={blogsVerticalTabsData} setSection={setSection} />
                     </div>
 
-                    <div className='col-span-8 gap-4'>
+                    <div className='lg:col-span-7 xl:col-span-8 gap-4'>
 
                         <div id="head1" className=''>
                             <div className="flex items-center w-full xl:min-w-[550px] mb-5">
@@ -36,25 +36,25 @@ const BlogDetail = () => {
                             </h2>
                             <p className="text-[16px] lg:text-[18px] text-justify lg:text-left">{blogsVerticalTabsData[0]?.description}</p>
 
-                            <div className='flex justify-around items-center py-9 px-5'>
+                            <div className='flex md:flex-row flex-col justify-around md:items-center gap-4 py-9 px-2 xl:px-5'>
                                 <div className='flex gap-3 items-center'>
                                     <div className='bg-hamzaPrimary/20 backdrop-blur-sm p-3 rounded-full w-[45px] h-[45px] flex items-center justify-center'>
                                         <Image src={BlogHand} alt={BlogHand} />
                                     </div>
-                                    <h3 className='max-w-24 font-semibold'>Lorem ipsum dolor sit</h3>
+                                    <h3 className='md:max-w-24 font-semibold'>Lorem ipsum dolor sit</h3>
                                 </div>
 
                                 <div className='flex gap-4 items-center'>
                                     <div className='bg-hamzaPrimary/20 backdrop-blur-sm p-3 rounded-full w-[45px] h-[45px] flex items-center justify-center'>
                                         <Image src={BlogHand} alt={BlogHand} />
                                     </div>
-                                    <h3 className='max-w-24 font-semibold'>Lorem ipsum dolor sit</h3>
+                                    <h3 className='md:max-w-24 font-semibold'>Lorem ipsum dolor sit</h3>
                                 </div>
                                 <div className='flex gap-4 items-center'>
                                     <div className='bg-hamzaPrimary/20 backdrop-blur-sm p-3 rounded-full w-[45px] h-[45px] flex items-center justify-center'>
                                         <Image src={BlogHand} alt={BlogHand} />
                                     </div>
-                                    <h3 className='max-w-24 font-semibold'>Lorem ipsum dolor sit</h3>
+                                    <h3 className='md:max-w-24 font-semibold'>Lorem ipsum dolor sit</h3>
                                 </div>
                             </div>
                         </div>
@@ -65,7 +65,7 @@ const BlogDetail = () => {
                             </h2>
 
                             <div className="relative">
-                                <div className="float-right w-[370px] ml-6 mb-4">
+                                <div className="sm:float-right sm:w-[300px] md:w-[370px] sm:ml-6 mb-2 md:mb-4">
                                     <Image
                                         src={BlogOurBenefits}
                                         alt="Blog benefits"
