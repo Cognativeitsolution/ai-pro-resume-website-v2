e2f4f9ba178d9706828c9d5cc2ba3096495a0295
HamzaAli198 <hamzvarin@outlook.com>
2025-04-23 05:48:43 -0700

    Api integration on resume example page

--- src/components/slider/AutoPlaySlider.tsx
+++ src/components/slider/AutoPlaySlider.tsx
@@ -31,7 +31,7 @@ export default function AutoPlaySlider(props: propsType) {
     Autoplay({ delay: 5000, stopOnFocusIn: false, stopOnInteraction: false }),
   ];
   const [emblaRef, emblaApi] = useEmblaCarousel(
-    { align: "center", dragFree: true, loop: true, ...options },
+    { align: "start", dragFree: true, loop: true, ...options },
     plugins // Ensure `plugins` has a valid type
   );
 
