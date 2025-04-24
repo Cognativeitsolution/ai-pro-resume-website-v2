5bf12d20ae7267d0d9c9548ca79a725039fac189
RabiaSid <rabiasid984@gmail.com>
2025-04-24 12:20:48 +0500

    set content resume format

--- src/components/resume-format/ResumeFormatComp.tsx
+++ src/components/resume-format/ResumeFormatComp.tsx
@@ -2,10 +2,11 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image';
 import verticleTabBg from 'media/images/verticleTabBg.webp'
-import IconContact from 'media/images/Icon.webp'
-import { BreadCrumbs, ProConsCard, QuickTips, DynVerticalTabs, CollapseTab, TemplatePoint, CTA } from '@/components'
+import { ProConsCard, DynVerticalTabs, CollapseTab, TemplatePoint, CTA } from '@/components'
 import DynBreadCrumbs from '../dynamic-pages/DynBreadCrumbs';
 import TipPro from 'media/images/TipPro.webp'
+import TempBg from 'media/resume_template_images/Template_24.webp'
+import DynPoints from '../dynamic-pages/DynPoints';
 
 type PointData = {
     icon: any
@@ -28,7 +29,8 @@ type ResumeDetailData = {
     templatePoint?: any,
     TemplatePointTab?: any,
     ResumeFormateTips?: any,
-    DynVerticalTabsData?: DynVerticalTabsData[]
+    DynVerticalTabsData?: DynVerticalTabsData[],
+    BlogIncludedPoints?: any,
     TemplateData?: any,
     headingTab?: any[]
 };
@@ -44,6 +46,7 @@ export default function ResumeFormatComp(props: propsType) {
     useEffect(() => {
         setSectionId("#head1")
     }, [])
+
     return (
         <section className="py-5 md:py-10">
             <div className="container">
@@ -55,25 +58,38 @@ export default function ResumeFormatComp(props: propsType) {
                             <DynVerticalTabs heading="Table Of Content"
                                 tabsData={data.DynVerticalTabsData}
                                 setSectionId={setSectionId} />
-                            <div className="relative  mt-4 hidden lg:block"
+                            <div className="mt-4 hidden lg:block relative z-10"
                             >
                                 <Image
                                     src={verticleTabBg}
                                     alt="Resume benefits"
                                 />
-                                <div className='left-[13%] bottom-12 absolute text-yellow-100'>
-                                    <h2 className='text-3xl font-semibold'>Need Help</h2>
-                                    <div className='flex text-center items-center gap-4 mt-4'>
-                                        <Image
-                                            src={IconContact}
-                                            alt="Resume benefits"
-                                            width={70}
-                                            height={70}
-                                        />
-                                        <div className='flex flex-col items-start'>
-                                            <p className='text-lg font-light'> Support </p>
-                                            <p className='text-xl'> +125 (405) 555-0128 </p>
+                                <div className='absolute z-20 top-0 w-full h-full flex flex-col items-center justify-center'>
+                                    <div className='flex items-center justify-center gap-4 border-b py-6'>
+                                        <div className=''>
+                                            <Image
+                                                src={TempBg}
+                                                alt="Resume benefits"
+                                                height={220}
+                                                className='rounded-md'
+
+
+                                            />
                                         </div>
+                                        <div className='flex flex-col items-center justify-center w-52'>
+                                            <p className='text-3xl font-semibold text-white'> Build your resume
+                                                in 15 Minutes </p>
+                                            <p className='text-sm text-white'> Use professional field-tested resume templates that follow the exact ‘resume rules’ employers look for.</p>
+                                        </div>
+                                    </div>
+                                    <div className='py-6'>
+                                        <CTA
+                                            btn
+                                            text="Create Resume"
+                                            bgColor="bg-primary hover:bg-PrimaryDark"
+                                            txtColor="text-white"
+                                            border="border-0"
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -82,15 +98,57 @@ export default function ResumeFormatComp(props: propsType) {
                     <div className='lg:col-span-7 xl:col-span-8'>
 
                         <div className='scroll-mt-32' id='head1'>
+                            <h2 className="text-[20px] md:text-[20px] lg:text-[20px] leading-[36px] md:leading-[40px] lg:leading-[50px] mb-2">
+                                Best Resume Formats to Impress in 2025
+                            </h2>
+                            <p className='text-base'>
+                                The first thing you choose when making a resume is a resume format. Selecting the resume format is important as it reflects the story of your professional experience to the employers.  This will also affect the chances of you getting your dream job.
+                            </p>
+                            <p className='mt-3 text-base'>But you must be wondering which resume would work the best for you? In this guide, we will assist you in deciding the best generic resume format so you can have a roadmap to write your resume further</p>
+
+                            <ProConsCard
+                                title="PRO TIP"
+                                icon={TipPro}
+                                description="Moreover, to grab the attention of your employers, you need to check all the elements of the best format for resume. These elements include a clear and well-structured resume. Such a resume will increase your chances of getting an interview. "
+                                isTip={true}
+                            />
+                        </div>
+
+                        <div className='scroll-mt-32' id='head2'>
+                            <h2 className="text-[22px] text-center border-b-2 md:text-[30px] lg:text-[34px] leading-[36px] md:leading-[40px] lg:leading-[50px] font-medium mb-4 capitalize mt-8">
+                                This blog will include
+                            </h2>
+                            <DynPoints includedPoints={data.BlogIncludedPoints} />
+
+                            <ProConsCard
+                                title="Need A resume ASAP?"
+                                icon={TipPro}
+                                description="Our AI-powered resume builder has features that can help you write a resume in time. With more than 30+ professional resume templates, you can choose the one that is suitable for your industry and begin creating a perfect resume. Also, you can revamp your old resume by importing it on our platform and customizing it the way you want. "
+                                isTip={true}
+                            />
+                            <div className='mt-8 mb-7 flex justify-center'>
+                                <CTA
+                                    btn
+                                    text="Create Resume"
+                                    bgColor="bg-primary hover:bg-PrimaryDark"
+                                    txtColor="text-white"
+                                    border="border-0"
+                                />
+                            </div>
+                        </div>
+
+                        <div className='scroll-mt-32' id='head3'>
                             <h2 className="text-[22px] md:text-[30px] lg:text-[34px] leading-[36px] md:leading-[40px] lg:leading-[50px] font-semibold mb-4">
                                 3 Most Popular Resume Formats
                             </h2>
+                            <p className="text-[16px] lg:text-[18px] text-justify">
+                                There are 3 main proper resume formats that are used around the world. Each format highlights your work experience and skills in a different way.
+                            </p>
                             <CollapseTab data={data.CollapseTab} />
                         </div>
 
-
                         {data?.TemplatePointTab?.map((items: any, index: number) => (
-                            <div className='scroll-mt-32 mb-8' id={`head${index + 2}`} key={index}>
+                            <div className='scroll-mt-32 mb-8' id={`head${index + 4}`} key={index}>
                                 <h2 className="text-[22px] md:text-[30px] lg:text-[34px] leading-[36px] md:leading-[40px] lg:leading-[50px] font-semibold ">
                                     {items.heading}
                                 </h2>
@@ -123,7 +181,7 @@ export default function ResumeFormatComp(props: propsType) {
                             </div>
                         ))}
 
-                        <div className='scroll-mt-32 pb-4' id='head5'>
+                        <div className='scroll-mt-32 pb-4' id='head7'>
                             <h2 className="text-[22px] md:text-[30px] lg:text-[34px] leading-[36px] md:leading-[40px] lg:leading-[50px] font-semibold ">
                                 Resume Formatting Tips For Writing A Perfect Resume
                             </h2>
@@ -141,7 +199,7 @@ export default function ResumeFormatComp(props: propsType) {
                             ))}
                         </div>
 
-                        <div className='scroll-mt-32' id='head6'>
+                        <div className='scroll-mt-32' id='head8'>
                             <h2 className="text-[22px] md:text-[30px] lg:text-[34px] leading-[36px] md:leading-[40px] lg:leading-[50px] font-semibold mb-4">
                                 Resume Templates That You Can Use To Grab an Employer’s Attention
                             </h2>
@@ -173,7 +231,7 @@ export default function ResumeFormatComp(props: propsType) {
                             ))}
                         </div>
 
-                        <div className='scroll-mt-32' id='head7'>
+                        <div className='scroll-mt-32' id='head9'>
                             <h2 className="text-[22px] md:text-[30px] lg:text-[34px] leading-[36px] md:leading-[40px] lg:leading-[50px] font-semibold mb-4">
                                 The Bottom Line
                             </h2>
