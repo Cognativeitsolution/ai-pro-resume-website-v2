f047580c7379487281e422f5bbf7f1e0cc0213a3
cognative-farooq <farooq.cognativeit@gmail.com>
2025-04-28 09:29:10 +0500

    all branches merged

---
+++ src/app/cover-letter-templates/page.tsx
@@ -0,0 +1,15 @@
+import { Faq, IconSlider, InnerBanner, PrivacyFeatures, ShowAllTemplates, Welcome } from "@/components";
+import { BannerData, faqs, FeaturesData, IconData, WelcomeData } from "./data";
+
+export default function Page() {
+  return (
+    <>
+      <InnerBanner {...BannerData} />
+      <IconSlider {...IconData} />
+      <ShowAllTemplates />
+      <Welcome data={WelcomeData} />
+      <PrivacyFeatures data={FeaturesData} />
+      <Faq data={faqs} />
+    </>
+  );
+}
