5c516ac67ec2cb4df8b9a907f94de71e6b3d886a
cognative-farooq <farooq.cognativeit@gmail.com>
2025-04-30 14:39:33 +0500

    dynamic sections

--- src/components/dynamicSection/DynamicSection.tsx
+++ src/components/dynamicSection/DynamicSection.tsx
@@ -1,86 +1,76 @@
-'use client'
-import React, { useEffect, useState } from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-// ==============
-import DynBreadCrumbs from '../dynamic-pages/DynBreadCrumbs';
-import { ProConsCard, CTA } from '@/components'
+// ===================
+import { CTA } from '@/components';
 import DynamicContent from './DynamicContent';
 import DynamicTabs from './DynamicTabs';
-// ==============
-import verticleTabBg from 'media/images/verticleTabBg.webp'
-import TempBg from 'media/resume_template_images/Template_24.webp'
+import DynBreadCrumbs from '../dynamic-pages/DynBreadCrumbs';
+// ===================
+import verticleTabBg from 'media/images/verticleTabBg.webp';
+import TempBg from 'media/resume_template_images/Template_24.webp';
 
-type PointData = {
-    icon: any
-    para: string
-}
-
-type DynVerticalTabsData = {
-    id?: string
-    name?: string
-    description?: React.ReactNode
-    points?: PointData[]
-    gridImages?: any[]
-    quickTip?: boolean
+type DynamicTabsData = {
+    id?: string;
+    title?: React.ReactNode;
 };
 
-type FormatDetail = {
-    DynVerticalTabsData?: DynVerticalTabsData[],
-    TopSection?: any
-    FormatPoints?: any,
-    CollapseTab?: any,
-    TemplatePointTab?: any,
-    FormatQuesAns?: any,
-    TemplateData?: any,
-    BottomSection?: any
+type PropsType = {
+    data: {
+        tip1?: any;
+        tip2?: any;
+        section1: any;
+        section2: any;
+        section3: any;
+        section4: any;
+        section5: any;
+        pros: any;
+        cons: any;
+        DynamicTabsData?: DynamicTabsData[];
+    };
 };
 
-type propsType = {
-    data: FormatDetail;
-};
-
-const DynamicSection = (props: propsType) => {
-    const { data } = props
-    const [sectionId, setSectionId] = useState("");
+const DynamicSection = ({ data }: PropsType) => {
+    const [sectionId, setSectionId] = useState('');
 
     useEffect(() => {
-        setSectionId("#head1")
-    }, [])
+        setSectionId('#section1');
+    }, []);
 
     return (
         <section className="py-5 md:py-10">
             <div className="container">
                 <DynBreadCrumbs />
-                <div className='relative grid lg:grid-cols-12 gap-4 xl:gap-6'>
+                <div className="relative grid lg:grid-cols-12 gap-4 xl:gap-8">
                     <div className="lg:col-span-5 xl:col-span-4 static">
                         <div className="sticky top-32 overflow-y-auto">
-                            <DynamicTabs heading="Table Of Content"
-                                tabsData={data?.DynVerticalTabsData}
-                                setSectionId={setSectionId} />
+                            <DynamicTabs
+                                heading="Table Of Content"
+                                tabsData={data?.DynamicTabsData}
+                                setSectionId={setSectionId}
+                            />
                             <div className="mt-4 hidden lg:block relative z-10">
-                                <Image
-                                    src={verticleTabBg}
-                                    alt="Resume benefits"
-                                />
-                                <div className='absolute z-20 top-0 w-full h-full flex flex-col items-center justify-center'>
-                                    <div className='flex items-center justify-center gap-4 border-b py-6'>
-                                        <div className=''>
+                                <Image src={verticleTabBg} alt="Resume benefits" />
+                                <div className="absolute z-20 top-0 w-full h-full flex flex-col items-center justify-center">
+                                    <div className="flex items-center justify-center gap-4 border-b py-6">
+                                        <div>
                                             <Image
                                                 src={TempBg}
-                                                alt="Resume benefits"
+                                                alt="Resume preview"
                                                 height={220}
-                                                className='rounded-md'
-
-
+                                                className="rounded-md"
                                             />
                                         </div>
-                                        <div className='flex flex-col items-center justify-center w-52'>
-                                            <p className='text-3xl font-semibold text-white'> Build your resume
-                                                in 15 Minutes </p>
-                                            <p className='text-sm text-white'> Use professional field-tested resume templates that follow the exact ‘resume rules’ employers look for.</p>
+                                        <div className="flex flex-col items-center justify-center w-52">
+                                            <p className="text-3xl font-semibold text-white">
+                                                Build your resume in 15 Minutes
+                                            </p>
+                                            <p className="text-sm text-white">
+                                                Use professional field-tested resume templates that follow the exact ‘resume rules’ employers look for.
+                                            </p>
                                         </div>
                                     </div>
-                                    <div className='py-6'>
+                                    <div className="py-6">
                                         <CTA
                                             btn
                                             text="Create Resume"
@@ -93,32 +83,13 @@ const DynamicSection = (props: propsType) => {
                             </div>
                         </div>
                     </div>
-                    <div className='lg:col-span-7 xl:col-span-8'>
-                        <DynamicContent
-                            id="head1"
-                            title={data?.TopSection?.heading}
-                            descBase={data?.TopSection?.description}
-                            titleTag="h2"
-                            titleClass="text-[22px] md:text-[30px] lg:text-[34px] leading-[36px] md:leading-[40px] lg:leading-[50px] font-semibold mb-2"
-                        >
-                        </DynamicContent>
-                        <ProConsCard
-                            title={data?.TopSection?.titlePro}
-                            icon={data?.TopSection?.iconPro}
-                            description={data?.TopSection?.descriptionPro}
-                            isTip={data?.TopSection?.isTip}
-                        />
-                        <ProConsCard
-                            title={data?.TopSection?.titlePro}
-                            icon={data?.TopSection?.iconPro}
-                            description={data?.TopSection?.descriptionPro}
-                            isTip={data?.TopSection?.isTip}
-                        />
+                    <div className="lg:col-span-7 xl:col-span-8">
+                        <DynamicContent id={sectionId} data={data} />
                     </div>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default DynamicSection   
+export default DynamicSection;
--- src/components/dynamicSection/DynamicTabs.tsx
+++ src/components/dynamicSection/DynamicTabs.tsx
@@ -1,6 +1,7 @@
 'use client'
-import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
+import Link from 'next/link';
+// ==============
 import { FaAngleDoubleRight } from 'react-icons/fa';
 
 type DynamicTabsData = {
@@ -13,7 +14,7 @@ type tabsData = {
     heading?: string;
 };
 const DynamicTabs = ({ heading, tabsData, setSectionId }: tabsData) => {
-    const [selectedTab, setSelectedTab] = useState<any>('#head1')
+    const [selectedTab, setSelectedTab] = useState<any>('#section1')
     const handleSelectedTab = (selectedId: any) => {
         setSelectedTab(selectedId)
         setSectionId(selectedId)
@@ -28,28 +29,25 @@ const DynamicTabs = ({ heading, tabsData, setSectionId }: tabsData) => {
 
     return (
         <>
-            <div className='p-4 xl:p-8 rounded-lg border-white border bg-indigo-200/20 backdrop-blur-none shadow-md '>
-                <h3 className='text-xl mb-4 font-semibold text-center'>{heading}</h3>
+            <div className='p-4 xl:p-6 rounded-lg border-white border bg-indigo-200/20 backdrop-blur-none shadow-md '>
+                <h3 className='text-xl mb-3 font-semibold text-center'>{heading}</h3>
                 <div className='divide-y-[1.5px] divide-[#ffffff]'>
                     {tabsData?.length && tabsData.map((data, index) => {
-                        const lastTab = index === tabsData?.length - 1;
                         return (
                             <Link
-                                key={data?.id}
+                                key={index}
                                 href={`#${data.id}`}
                                 scroll={true}
                                 onClick={() => handleSelectedTab(data?.id)}
-                                className={`flex pt-3 ${lastTab ? 'pb-3 mb-0' : 'pb-3'}
-                                gap-6 cursor-pointer  items-center
+                                className={`flex py-3 gap-5 cursor-pointer items-center ${index === tabsData.length - 1 && selectedTab !== data?.id ? 'pb-0' : 'pb-3'}
                                 ${selectedTab === data?.id ? 'bg-PrimaryDark rounded-lg ps-5 text-white' : ''}`}
                             >
                                 <span className='text-lg font-light'><FaAngleDoubleRight /></span>
-                                <p className=''>{data?.title}</p>
+                                <p>{data?.title}</p>
                             </Link>
                         )
                     })}
                 </div>
-
             </div>
         </>
 
